456ae0d9d2cc9ab444cf27d6b65adeb818c15e09
tamenze <tamenze@gmail.com>
2025-11-09 18:15:28 -0500

    decrease connections

--- config/puma.rb
+++ config/puma.rb
@@ -24,11 +24,24 @@
 # Any libraries that use a connection pool or another resource pool should
 # be configured to provide at least as many connections as the number of
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
-threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+# threads & workers
+max_threads = ENV.fetch("RAILS_MAX_THREADS", 2).to_i
+threads max_threads, max_threads
+
+workers ENV.fetch("WEB_CONCURRENCY", 1).to_i
+preload_app!
+
+# Bind to Render's injected port
+port ENV.fetch("PORT", 10000)
+bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 10000)}"
+
+environment ENV.fetch("RAILS_ENV", "production")
+
+on_worker_boot do
+  # Make sure each worker (re)connects to the DB
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
